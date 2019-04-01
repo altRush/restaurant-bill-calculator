98e2abd9ce5c04062b2723b20bcf751709e45176
Rush WP <rushing.wp@gmail.com>
2019-04-01 12:45:24 +0700

    Coupon case's hierarchy is now working. Modulize them next.

--- couponCases.js
+++ couponCases.js
@@ -1,33 +1,34 @@
-if (estimatedBill < 6000) {
-  if (couponCode === 'LUCKY ONE' || personCount * 459 >= 1000) {
-    bill.billValue = Math.floor(
-      personCount * 459 - (personCount * 459 * 15) / 100
-    );
-  } else if (couponCode === 'LUCKY TWO') {
-    bill.billValue = Math.floor(
-      personCount * 459 - (personCount * 459 * 20) / 100
-    );
-  } else if (couponCode === '4PAY3') {
-    if (personCount > 3) {
-      let personDeduct = personCount / 4;
-      let exceedingPersonRatio = personDeduct % 1;
-      let personDuringDisount = 0;
+if (couponCode === 'LUCKY ONE') {
+  bill.billValue = Math.floor(
+    personCount * 459 - (personCount * 459 * 15) / 100
+  );
+} else if (couponCode === 'LUCKY TWO') {
+  bill.billValue = Math.floor(
+    personCount * 459 - (personCount * 459 * 20) / 100
+  );
+} else if (couponCode === '4PAY3') {
+  if (personCount > 3) {
+    let personDeduct = personCount / 4;
+    let exceedingPersonRatio = personDeduct % 1;
+    let personDuringDisount = 0;
 
-      if (exceedingPersonRatio !== 0) {
-        personDeduct = Math.floor(personDeduct);
-      }
-
-      personDuringDisount = personCount - personDeduct;
-      bill.personFinalCount = personDuringDisount;
-      bill.billValue = Math.floor(bill.personFinalCount * 459);
-    } else {
-      bill.billValue = Math.floor(personCount * 459);
-      console.log('This coupon code is not applicable for you.');
+    if (exceedingPersonRatio !== 0) {
+      personDeduct = Math.floor(personDeduct);
     }
+
+    personDuringDisount = personCount - personDeduct;
+    bill.personFinalCount = personDuringDisount;
+    bill.billValue = Math.floor(bill.personFinalCount * 459);
   } else {
     bill.billValue = Math.floor(personCount * 459);
+    console.log('This coupon code is not applicable for you.');
   }
 } else {
+  bill.billValue = Math.floor(personCount * 459);
+}
+
+if (estimatedBill > 6000) {
+  bill.couponStack.length = 0;
   console.log(
     (bill.billValue = Math.floor(
       personCount * 459 - (personCount * 459 * 25) / 100
--- src/components/CouponForm.js
+++ src/components/CouponForm.js
@@ -23,31 +23,30 @@ export default class CouponForm extends Component {
     const bill = {
       personCount: parseInt(this.state.personCount),
       billValue: this.state.billValue,
-      couponCode: this.state.couponCode,
       couponStack: this.state.couponStack
     };
 
-    let { couponCode, couponStack, personCount } = bill;
-    let estimatedBill = personCount * 459;
+    const couponCode = this.state.couponCode;
+    let { couponStack, personCount } = bill;
     // The assignment said these rules can be in a combination, need to separate them apart as objects.
 
-    if (couponCode === 'LUCKY ONE' || estimatedBill >= 1000) {
+    if (couponCode === 'LUCKY ONE') {
       if (couponStack.indexOf('LUCKY ONE') === -1) {
         couponStack.push('LUCKY ONE');
+        // console.log(bill);
       } else {
         console.log('"LUCKY ONE" coupon code is already applied!');
       }
-      console.log(couponStack);
     }
 
     if (couponCode === 'LUCKY TWO') {
       if (personCount > 1) {
         if (couponStack.indexOf('LUCKY TWO') === -1) {
           couponStack.push('LUCKY TWO');
+          // console.log(bill);
         } else {
           console.log('"LUCKY TWO" coupon code is already applied!');
         }
-        console.log(couponStack);
       } else {
         console.log('This coupon code is not applicable for you.');
       }
@@ -57,14 +56,104 @@ export default class CouponForm extends Component {
       if (personCount > 3) {
         if (couponStack.indexOf('4PAY3') === -1) {
           couponStack.push('4PAY3');
+          // console.log(bill);
         } else {
           console.log('"4PAY3" coupon code is already applied!');
         }
-        console.log(couponStack);
       } else {
         console.log('This coupon code is not applicable for you.');
       }
     }
+
+    // Coupon activation
+    let estimatedBill = personCount * 459;
+
+    const lucky = estimatedBill => {
+      if (couponStack.indexOf('LUCKY TWO') !== -1) {
+        estimatedBill = Math.floor(estimatedBill - (estimatedBill * 20) / 100);
+        console.log(`This is LUCKY TWO`);
+      } else if (couponStack.indexOf('LUCKY ONE') !== -1) {
+        estimatedBill = Math.floor(estimatedBill - (estimatedBill * 15) / 100);
+        console.log(`This is LUCKY ONE`);
+      }
+      return estimatedBill;
+    };
+
+    const fourPayThree = estimatedBill => {
+      if (personCount > 3) {
+        let personDeduct = personCount / 4;
+        let exceedingPersonRatio = personDeduct % 1;
+        let personDuringDisount = 0;
+
+        if (exceedingPersonRatio !== 0) {
+          personDeduct = Math.floor(personDeduct);
+        }
+
+        personDuringDisount = personCount - personDeduct;
+        bill.personFinalCount = personDuringDisount;
+        console.log('This is 4PAY3');
+        estimatedBill = Math.floor(bill.personFinalCount * 459);
+      } else {
+        estimatedBill = Math.floor(estimatedBill);
+        console.log('This coupon code is not applicable for you.');
+      }
+      return estimatedBill;
+    };
+
+    let couponCase = '';
+    if (
+      (couponStack.indexOf('LUCKY ONE') !== -1 ||
+        couponStack.indexOf('LUCKY TWO') !== -1) &&
+      couponStack.indexOf('4PAY3') === -1
+    ) {
+      couponCase = 'lucky';
+    } else if (
+      couponStack.indexOf('LUCKY ONE') === -1 &&
+      couponStack.indexOf('LUCKY TWO') === -1 &&
+      couponStack.indexOf('4PAY3') !== -1
+    ) {
+      couponCase = 'fourPayThree';
+    } else if (
+      (couponStack.indexOf('LUCKY ONE') !== -1 ||
+        couponStack.indexOf('LUCKY TWO') !== -1) &&
+      couponStack.indexOf('4PAY3') !== -1
+    ) {
+      couponCase = 'luckyFourPayThree';
+    } else {
+      couponCase = 'No case';
+    }
+
+    switch (couponCase) {
+      case 'lucky':
+        estimatedBill = lucky(estimatedBill);
+        break;
+      case 'fourPayThree':
+        estimatedBill = fourPayThree(estimatedBill);
+        break;
+      case 'luckyFourPayThree':
+        estimatedBill = lucky(fourPayThree(estimatedBill));
+        break;
+      default:
+        return estimatedBill;
+    }
+
+    console.log(`Estimated bill: ${estimatedBill}`);
+
+    // if (estimatedBill > 6000) {
+    //   bill.couponStack.length = 0;
+    //   console.log(
+    //     (bill.billValue = Math.floor(
+    //       estimatedBill - (estimatedBill * 25) / 100
+    //     ))
+    //   );
+    // } else {
+    //   console.log(estimatedBill);
+
+    // }
+
+    bill.billValue = estimatedBill;
+
+    console.log(`Grand total: `);
     console.log(bill);
   }
   render() {
